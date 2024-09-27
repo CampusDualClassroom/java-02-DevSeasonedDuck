f65e79b028bde736c5ce3885cb3e6edb1e06f767
Diego Alonso Cárcamo Gutiérrez <diegocarcamo.contacto@gmail.com>
2024-09-27 09:47:23 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise02.java
+++ src/main/java/com/campusdual/classroom/Exercise02.java
@@ -1,17 +1,19 @@
 package com.campusdual.classroom;
 
+
 public class Exercise02 {
 
     public static void main(String[] args) {
         double r = 15;
-        double a, l;
+        double l = 0;
 
-        // TODO: ↓ Fórmula del área de un círculoS: a = radio * radio * Número PI
+        double a = 3.14*Math.pow(r,2);
 
         System.out.println("Area: " + a);
 
         // TODO: ↓ Fórmula de la longitud de una circunferencia: l = 2 * radio * Número PI
 
+        l=2*r*3.14;
         System.out.println("Longitud:  " + l);
 
     }
